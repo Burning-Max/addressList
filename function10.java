64f942263453e74958d2c57ce6a5f1e33ff7cf2a
hdliu61 <270094542@qq.com>
2015-06-11 13:08:39 +0800

    liuyu

---
+++ function10.java
@@ -0,0 +1,14 @@
+class function10 implements common
+{
+	public void function10(){
+	}
+	public void open(){
+		System.out.println("Hi,这是一个通讯录管理工具。");
+		System.out.println("首先您会进入登录模块，包含注册，登录，注销功能，");
+		System.out.println("进入后显示菜单，菜单为序号+功能，键入序号选择该功能，");
+		System.out.println("每次实现该功能后，提示再次选择该功能还是返回菜单，选择退出功能后程序结束。");
+	}
+	public void close(){
+		System.out.println("退出了");
+	}
+}
